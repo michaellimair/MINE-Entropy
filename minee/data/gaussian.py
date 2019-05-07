9b70765321902f5372e6c29b9f967c9bb64c2b69
ThomasHuang168 <huanghingpang@gmail.com>
2019-05-07 15:28:50 +0800

    change gaus ground truth expression and add main_Ali

--- minee/data/gaussian.py
+++ minee/data/gaussian.py
@@ -74,35 +74,68 @@ class Gaussian():
 
 
 if __name__ == '__main__':
-    rho = 1-(1e-3)
-    gaus=Gaussian(200, 0, 1, rho)
-    # data = gaus.data
-    # import os
-    # import matplotlib
-    # # matplotlib.use('Agg')
-    # import matplotlib.pyplot as plt
+    # rho = 1-(1e-3)
+    # gaus=Gaussian(200, 0, 1, rho)
+    # # data = gaus.data
+    import os
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
         
-    # #Plot Ground Truth MI
-    # fig, axs = plt.subplots(1, 2, figsize=(45, 15))
-    # ax = axs[0]
-    # ax.scatter(data[:,0], data[:,1], color='r', marker='o')
+    # # #Plot Ground Truth MI
+    # # fig, axs = plt.subplots(1, 2, figsize=(45, 15))
+    # # ax = axs[0]
+    # # ax.scatter(data[:,0], data[:,1], color='r', marker='o')
 
-    # ax = axs[1]
-    # Xmax = max(data[:,0])+1
-    # Xmin = min(data[:,0])-1
-    # Ymax = max(data[:,1])+1
-    # Ymin = min(data[:,1])-1
-    # x = np.linspace(Xmin, Xmax, 300)
-    # y = np.linspace(Ymin, Ymax, 300)
-    # xs, ys = np.meshgrid(x,y)
-    # ax, c = gaus.plot_i(ax, xs, ys)
-    # fig.colorbar(c, ax=ax)
-    # ax.set_title("i(X;Y)")
-    # figName = os.path.join("minee/experiments", "guas_rho=0.5_i_XY.png")
-    # fig.savefig(figName, bbox_inches='tight')
-    # plt.close()
+    # # ax = axs[1]
+    # # Xmax = max(data[:,0])+1
+    # # Xmin = min(data[:,0])-1
+    # # Ymax = max(data[:,1])+1
+    # # Ymin = min(data[:,1])-1
+    # # x = np.linspace(Xmin, Xmax, 300)
+    # # y = np.linspace(Ymin, Ymax, 300)
+    # # xs, ys = np.meshgrid(x,y)
+    # # ax, c = gaus.plot_i(ax, xs, ys)
+    # # fig.colorbar(c, ax=ax)
+    # # ax.set_title("i(X;Y)")
+    # # figName = os.path.join("minee/experiments", "guas_rho=0.5_i_XY.png")
+    # # fig.savefig(figName, bbox_inches='tight')
+    # # plt.close()
 
-    # plt.scatter(data[:,0], data[:,1])
-    print(gaus.ground_truth)
-    print(-0.5*np.log(1-rho*rho))
+    # # plt.scatter(data[:,0], data[:,1])
+    # print(gaus.ground_truth)
+    # print(-0.5*np.log(1-rho*rho))
     # plt.show()
+
+    eq = []
+    integral = []
+    rhos = []
+    log_rhos = []
+    for i in range(12):
+        rho = 1 - 1/(10**i)
+        rhos.append(rho)
+        log_rhos.append(-i)
+        # print(rho)
+        gaus=Gaussian(200,0,1,rho)
+        integral.append(gaus.ground_truth)
+        eq.append(-0.5*np.log(1-rho*rho))
+    fig, axs = plt.subplots(1, 2, figsize=(16*2,9))
+    ax = axs[0]
+    ax.plot(rhos, eq, color='r', marker='o', label='equation')
+    ax.plot(rhos, integral, color='g', marker='x', label='integral')
+    ax.legend()
+    ax.set_xlabel('rho')
+    ax.set_ylabel('mutual information')
+    ax.set_title("mi ground truth comparison")
+
+    ax = axs[1]
+    ax.plot(log_rhos, eq, color='r', marker='o', label='equation')
+    ax.plot(log_rhos, integral, color='g', marker='x', label='integral')
+    ax.legend()
+    ax.set_xlabel('log(1-rho)')
+    ax.set_ylabel('mutual information')
+    ax.set_title("mi ground truth comparison")
+
+    figName = os.path.join("minee/experiments", "mi_ground_truth_compare_Test.png")
+    fig.savefig(figName, bbox_inches='tight')
+    plt.close()
